6bb8e6cbfa05312ee90cda80a3d70b20a5e6e7d9
Xavier Perseguers <xavier@causal.ch>
2016-10-17 23:53:26 +0200

    [TASK] Refactor initialization with best practices

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,10 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-// Register routing service
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Controller/RoutingController.php';
+$boot = function ($_EXTKEY) {
+    // Register routing service
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Controller/RoutingController.php';
+};
+
+$boot($_EXTKEY);
+unset($boot);
